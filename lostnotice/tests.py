19fac879a34a7a25170a0d6aa77c44043b895602
spFrank01 <s5801012630149@email.kmutnb.ac.th>
2017-07-10 07:53:17 -0700

    unittest, test show list in homepage

--- lostnotice/tests.py
+++ lostnotice/tests.py
@@ -24,5 +24,63 @@ class HomePageTest(TestCase):
 		self.assertTrue(response.content.strip().endswith(b'</html>'))
 
 
+class ShowListInHomepageTest(TestCase):
+	# LFList = LostFindList
+	# baseUrl/lostNoticeListPage/findOwnerListPage
+	# Ex. self.client.get('/2/1') = baseUrl/2/1
+	#	page of lost notice list = 2
+	#	page of finw owner list  = 1
+
+	def test_show_lost_notice_list(self):
+		LostNoticeList.objects.create(title="First_List")
+		response = self.client.get('/')
+		self.assertIn(
+			'<a href="/lost_notice/1/" class="btn btn-link">First_List</a>', 
+			response.content.decode()
+		)
+		self.assertNotIn('<a href="/lost_notice/2/"', response.content.decode())
+
+	def test_show_link_next_to_page_2_of_lost_notice_list(self):
+		LostNoticeList.objects.create(title="First_List")
+		LostNoticeList.objects.create(title="Second_List")
+		LostNoticeList.objects.create(title="Third_List")
+		response = self.client.get('/')
+		self.assertIn('<a href="/LFList/2/1/"', response.content.decode())
+		self.assertNotIn('<a href="/LFList/1/2/"', response.content.decode())
+
+	def test_show_link_back_to_page_1_of_lost_notice_list(self):
+		LostNoticeList.objects.create(title="First_List")
+		LostNoticeList.objects.create(title="Second_List")
+		LostNoticeList.objects.create(title="Third_List")
+		response = self.client.get('/LFList/2/1/')
+		self.assertIn('<a href="/LFList/1/1/"', response.content.decode())
+
+
+	def test_show_find_owner_list(self):
+		FindOwnerList.objects.create(title="First_List")
+		response = self.client.get('/')
+		self.assertIn(
+			'<a href="/found_owner/1/" class="btn btn-link">First_List</a>', 
+			response.content.decode()
+		)
+		self.assertNotIn('<a href="/found_owner/2/"', response.content.decode())
+
+	def test_show_link_next_to_page_2_of_find_owner_list(self):
+		FindOwnerList.objects.create(title="First_List")
+		FindOwnerList.objects.create(title="Second_List")
+		FindOwnerList.objects.create(title="Third_List")
+		response = self.client.get('/')
+		self.assertIn('<a href="/LFList/1/2/"', response.content.decode())
+		self.assertNotIn('<a href="/LFList/2/1/"', response.content.decode())
+
+	def test_show_link_back_to_page_1_of_find_owner_list(self):
+		FindOwnerList.objects.create(title="First_List")
+		FindOwnerList.objects.create(title="Second_List")
+		FindOwnerList.objects.create(title="Third_List")
+		response = self.client.get('/LFList/1/2/')
+		self.assertIn('<a href="/LFList/1/1/"', response.content.decode())
+
+
+
 
 		
